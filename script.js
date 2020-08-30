98cbd482fe475c3db649b6aae60e540fbe7f17e8
David Moler <tigermoler@gmail.com>
2020-08-29 17:31:48 -0700

    No longer getting undefined in text value, still not fully random

--- script.js
+++ script.js
@@ -1,13 +1,7 @@
-// Constants
 const generateBtn = document.querySelector("#generate");
 
-// Functions
 
 
-
-/**
- * writes a generated password to the #password
- */
 function writePassword() {
   const password = generatePassword();
   const passwordText = document.querySelector("#password");
@@ -15,9 +9,7 @@ function writePassword() {
   passwordText.value = password;
 }
 
-/**
- * generate a password based on certain criteria
- */
+
 function generatePassword() {
 
   let lower = "abcdefghijklmnopqrstuvwxyz"
@@ -30,7 +22,6 @@ function generatePassword() {
   let specArr = spec.split("");
   let allChars = [];
 
-  // Password is currently blank! We need to make a better one
   let password = "";
 
   let pwlength = prompt("Choose password length: 8-128 characters.");
@@ -39,30 +30,24 @@ function generatePassword() {
     alert("Password must be between defined length.")
     generatePassword()
   }
-
   if (confirm("Do you want lowercase characters?")) {
     allChars.push(lowerArr);
   }
-
   if (confirm("Do you want uppercase characters?")) {
     allChars.push(upperArr);
   }
-
   if (confirm("Do you wamt numeric characters?")) {
     allChars.push(numArr);
   }
-
   if (confirm("Do you want special characters?")) {
     allChars.push(specArr);
   }
-
   if (allChars.length === 0) {
     alert("Minimum of one type of character must be chosen");
     generatePassword()
   }
-
-  for (let i = 0; i < pwlength.length; ++i) {
-    let random = Math.floor(Math.random().pwlength);
+  for (let i = 0; i < pwlength; i++) {
+    let random = Math.floor(Math.random(allChars.length));
     password = allChars[random];
   }
 
@@ -70,8 +55,6 @@ function generatePassword() {
 }
 
 
-// Main Process
 
-// Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
